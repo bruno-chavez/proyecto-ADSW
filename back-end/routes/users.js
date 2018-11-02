c529d38e20f9ed488e31fc30e27b1d0da366b2f1
bruno.chavez <darklaier@gmail.com>
2018-11-02 09:29:35 -0300

    CORS habilitado para todas las rutas.

--- back-end/routes/users.js
+++ back-end/routes/users.js
@@ -2,7 +2,14 @@
 
 let express = require('express');
 let router = express.Router();
+let cors = require('cors');
 let userController = require('../controllers/userController');
+
+let app = express();
+
+app.use(cors({
+    origin: 'http://localhost:4200'
+}));
 
 /* GET users listing. */
 router.get('/', userController.list);
