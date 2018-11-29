186390c633cd4f347233c02b6bbb31a7ce843f4d
bruno.chavez <darklaier@gmail.com>
2018-11-29 17:18:36 -0300

    modelo y migracion de dispositivo actualizado.

--- back-end/models/device.js
+++ back-end/models/device.js
@@ -2,7 +2,7 @@
 
 module.exports = (sequelize, DataTypes) => {
   const Device = sequelize.define('Device', {
-    type: DataTypes.STRING,
+    name: DataTypes.STRING,
     description: DataTypes.STRING
   }, {
       freezeTableName: true,
