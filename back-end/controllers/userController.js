3ce127a7df13c78ece044fff76027b14102b78c6
bruno.chavez <darklaier@gmail.com>
2018-11-30 20:14:18 -0300

    ruta y controlador de usuario actualizado.

--- back-end/controllers/userController.js
+++ back-end/controllers/userController.js
@@ -3,15 +3,6 @@
 let {User} = require('../models');
 
 module.exports.post = function (req, res) {
-
-    User.create({
-        name:req.body.name,
-        email:req.body.email,
-        password:req.body.password
-    }).then(function (result) {
-        res.json(result);
-    });
-
     console.log(req.body, "req.body");
 
 };
--- back-end/routes/users.js
+++ back-end/routes/users.js
@@ -4,6 +4,6 @@ let express = require('express');
 let router = express.Router();
 let userController = require('../controllers/userController');
 
-router.post('/', userController.list);
+router.post('/', userController.post);
 
 module.exports = router;
