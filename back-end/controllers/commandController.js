b95621aed8d2dc9afb60837e0ad481e7bbea899a
bruno.chavez <darklaier@gmail.com>
2018-11-30 20:21:42 -0300

    controlador de comando actualizado.

--- back-end/controllers/commandController.js
+++ back-end/controllers/commandController.js
@@ -5,12 +5,11 @@ let {Command} = require('../models');
 module.exports.post = function (req, res) {
 
     Command.create({
-        value:req.body.valor,
-        type:req.body.tipo
+        value: req.body.valor,
+        type: req.body.tipo,
+        device: req.body.dispositivo,
+        function: req.body.funcion
         }).then(function (result) {
             res.json(result);
     });
-
-    console.log(req.body, "req.body");
-    //res.writeHead(200, {'Content-Type': 'application/json'});
 };
