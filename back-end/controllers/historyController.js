d78744db92fb399e2f887a7f1ade994d64efa190
bruno.chavez <darklaier@gmail.com>
2018-12-01 11:06:46 -0300

    controlador de historial actualizado

--- back-end/controllers/historyController.js
+++ back-end/controllers/historyController.js
@@ -4,6 +4,11 @@ let {User} = require('../models');
 
 module.exports.get = function (req, res) {
 
+    User.findAll({where: {id: req.params.id}}).then( function(data) {
+        let user = data[0];
+        res.json(user);
+    });
+
     console.log(req.params.id, 'hoi');
 
 };
