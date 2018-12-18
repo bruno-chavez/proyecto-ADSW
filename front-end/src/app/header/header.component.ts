fb6c488de76466d107683c7d8799c35642ff2665
bruno.chavez <darklaier@gmail.com>
2018-12-18 18:55:40 -0300

    servicio de header agregado.

--- front-end/src/app/header/header.component.ts
+++ front-end/src/app/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from "@angular/router";
+import { HeaderService } from "./header.service";
+import { User} from "../user/user";
 
 @Component({
   selector: 'app-header',
@@ -8,11 +10,23 @@ import {Router} from "@angular/router";
 })
 export class HeaderComponent implements OnInit {
 
-  constructor(private router:Router) { }
+  constructor(
+    private router:Router,
+    private headerService: HeaderService
+  ) { }
 
   ngOnInit() {
+    console.log("im on ngoniintit");
+    this.GetSession()
   }
   showLogin(){
     this.router.navigate(['/']);
+  }
+
+  GetSession(){
+    console.log("im getting sessioned");
+
+    let usuario = this.headerService.getSession();
+    console.log('usuario!', usuario);
   }
 }
