6b2f4d669078cd5a802de65bcf1cb7d0bbed19ad
bruno.chavez <darklaier@gmail.com>
2018-12-22 18:50:10 -0300

    valor de commando ahora almacena numeros decimales.

--- back-end/migrations/20181018013635-create-command.js
+++ back-end/migrations/20181018013635-create-command.js
@@ -10,7 +10,7 @@ module.exports = {
                 type: Sequelize.INTEGER
             },
             value: {
-                type: Sequelize.INTEGER
+                type: Sequelize.FLOAT
             },
             unit: {
                 type: Sequelize.STRING
